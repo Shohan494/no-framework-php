c7db511d38ceeb7fe7259f2eacf0634252deceac
shohan494 <shohan12224943@gmail.com>
2019-11-10 00:11:16 +0600

    php/di installed

---
+++ src/AwesomeClass.php
@@ -0,0 +1,14 @@
+class AwesomeClass
+{
+    private $dbConnection;
+
+    public function __construct(\PDO $dbConnection)
+    {
+        $this->dbConnection = $dbConnection;
+    }
+
+    public function doSomethingAwesome()
+    {        
+        // Make magic happen with $this->dbConnection
+    }
+}
